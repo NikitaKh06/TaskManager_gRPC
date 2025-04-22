3d319293abf3fc2559e8b01aa21fc6a23a4323d8
NikitaKh06 <nikitakhripunkov@mail.ru>
2025-04-22 16:48:41 +0300

    Finished gRPC application with two microservices

--- Task-Manager-API/main.go
+++ Task-Manager-API/main.go
@@ -4,17 +4,20 @@ import (
 	task_manager_grpc_generated "github.com/NikitaKh06/TaskManagerProtoFiles/github.com/NikitaKh06/TaskManagerProtoFiles/task-manager-grpc-generated"
 	"google.golang.org/grpc"
 	"log"
+	"net/http"
+	"os"
 	"task-manager-api/config"
 	"task-manager-api/internal"
 )
 
 func main() {
+	log.SetOutput(os.Stderr)
 	err := config.ConfigureLogger()
 	if err != nil {
 		log.Fatal(err)
 	}
-	
-	connection, err := grpc.NewClient("localhost:8080")
+
+	connection, err := grpc.Dial("task-manager-database-service-grpc:8080", grpc.WithInsecure())
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -22,4 +25,10 @@ func main() {
 
 	internal.ClientgRPC = task_manager_grpc_generated.NewTaskManagerClient(connection)
 
+	http.HandleFunc("/create", internal.CreateHandler)
+	http.HandleFunc("/list", internal.ListHandler)
+	http.HandleFunc("/delete", internal.DeleteHandler)
+	http.HandleFunc("/done", internal.DoneHandler)
+
+	http.ListenAndServe(":8081", nil)
 }
